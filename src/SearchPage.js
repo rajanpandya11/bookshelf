55ea43f699fa2ba91464005e8aef3420456915cc
rajan pandya <rajankpandya@gmail.com>
2020-03-28 13:36:19 -0400

    added some comments

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -15,36 +15,41 @@ class SearchPage extends Component{
         }
     }
 
-    handleSearch =(event) => {
-        let query = event.target.value.toLowerCase();
+    /**
+    * @description this function fetches books based on given search terms
+    * @param {string} query - query is what user types in to input field
+    */
+    handleSearch = (query) => {
+        query = query.toLowerCase();
         if(query.length > 2){
             BooksAPI.search(query).then((books) => {
                 if(books.length === 0) { this.setState({ searchTerm: query, searchedBooks: []}); }
-                if(books && books.length > 0) { this.setState({ searchTerm: query, searchedBooks: books }); } 
+                if(books && books.length > 0) {
+                    console.log('search : ', query);
+                    console.log('after search returns: ', books); 
+                    this.setState({ searchTerm: query, searchedBooks: books }); 
+                } 
             });
         } 
         if(query.length >= 0){
             this.setState({searchTerm: query});            
         }
-
     }
 
-    getBooks = () => {
-        let query = this.state.searchTerm.toLowerCase();
-        if(query.length > 2){
-            BooksAPI.search(query).then((books) => {
-                if(books.length === 0) { this.setState({searchedBooks: []}); }
-                if(books && books.length > 0) { 
-                    this.setState({ searchedBooks: books }); 
-                } 
-            });
-        }        
-    }
 
+    /**
+    * @description this function updates shelf for a book, when a new shelf is clicked from UI in book's drop down menu 
+    * @param {string} newShelf - is the value for shelf key to be updated for the book object
+    * @param {object} book - the book object
+    */
     handleChange = (newShelf, book) => {
         BooksAPI.update(book, newShelf).then((res) => console.log(res)).then( ()=> {this.props.updateBookList(book)} );
     }
 
+    /**
+    * @description this function finds, based on a book id, whether a book is assigned a shelf, by user 
+    * @param {object} book - the book object
+    */
     findTheShelf = (book) => {
         this.props.bookList.some((b) => {
             if(b.id === book.id){
@@ -66,35 +71,29 @@ class SearchPage extends Component{
                 <div className="search-books-bar">
                     <Link className="close-search" to="/">Close</Link>                
                     <div className="search-books-input-wrapper">
-                        {/*
-                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                        You can find these search terms here:
-                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                        you don't find a specific author or title. Every search is limited by search terms.
-                        */}
-                        <input type="text" placeholder="Search by title or author" value={this.state.searchTerm} onChange={this.handleSearch}/>
+                        <input type="text" placeholder="Search by title or author" value={this.state.searchTerm} onChange={(e) => this.handleSearch(e.target.value)}/>
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    {
+                        <ol className="books-grid">
                         {
-                            <ol className="books-grid">
-                            {
-                                this.state.searchedBooks.map((book, item) => {
-                                 const imagelink = book.imageLinks !== undefined ? book.imageLinks['thumbnail'] : '';
-                                 book.imagelink = imagelink;
-                                 book.shelf = this.findTheShelf(book);
-                                 return  (  
-                                     <li key={item+1}>
-                                         <Book shelves={bookShelves} book={book} handleChange={(a,b) => this.handleChange(a,b)}/>
-                                     </li>
-                                 );
-                                 })
-                            }
-                            </ol>
+                            this.state.searchedBooks.map((book, item) => {
+                                const imagelink = book.imageLinks !== undefined ? book.imageLinks['thumbnail'] : '';
+                                book.imagelink = imagelink;
+                                book.shelf = this.findTheShelf(book);
+                                return  (  
+                                    <li key={item+1}>
+                                        <Book 
+                                            shelves={bookShelves} 
+                                            book={book} 
+                                            handleChange={(a,b) => this.handleChange(a,b)} />
+                                    </li>
+                                );
+                                })
                         }
-                    </div>
-                    </div>
-                    <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                        </ol>
+                    }
                 </div>
             </div>
 
@@ -104,3 +103,20 @@ class SearchPage extends Component{
 }
 
 export default SearchPage;
+
+
+
+
+
+// getBooks = () => {
+//     let query = this.state.searchTerm.toLowerCase();
+//     if(query.length > 2){
+//         BooksAPI.search(query).then((books) => {
+//             if(books.length === 0) { this.setState({searchedBooks: []}); }
+//             if(books && books.length > 0) { 
+
+//                 this.setState({ searchedBooks: books }); 
+//             } 
+//         });
+//     }        
+// }
